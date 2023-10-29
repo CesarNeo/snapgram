b057e598f50a5f5230220e9bd9ee250dae9c2200
Cesar Emmanuel <cesaremmmanul@gmail.com>
2023-10-29 15:33:26 -0300

    feat: create sign up page and create user in appwrite

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,23 @@
+import { Navigate, Outlet } from 'react-router-dom'
+
 function AuthLayout() {
-  return <div>Enter</div>
+  const isAuthenticated = false
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center py-10 flex-col">
+            <Outlet />
+          </section>
+
+          <img src="/assets/images/side-img.svg" alt="logo" className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat' />
+        </>
+      )}
+    </>
+  )
 }
 
 export default AuthLayout
