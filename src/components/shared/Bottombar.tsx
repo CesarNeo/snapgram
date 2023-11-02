2a952910fb6201fd13649ec2f4216dc498fc5db2
Cesar Emmanuel <cesaremmmanul@gmail.com>
2023-11-02 17:05:40 -0300

    feat: create Bottombar in mobile version

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,5 +1,31 @@
+import { BOTTOMBAR_LINKS } from '@/constants'
+import { Link, useLocation } from 'react-router-dom'
+
 function Bottombar() {
-  return <div>Enter</div>
+  const { pathname } = useLocation()
+
+  return (
+    <footer className="bottom-bar">
+      {BOTTOMBAR_LINKS.map((link) => (
+        <Link
+          to={link.route}
+          key={link.label}
+          className="data-[active=true]:bg-primary-500 rounded-[10px] flex-center flex-col gap-1 p-2 transition"
+          data-active={pathname === link.route}
+        >
+          <img
+            src={link.imgURL}
+            alt={link.label}
+            data-active={pathname === link.route}
+            className="group-hover:invert-white transition-all data-[active=true]:invert-white"
+            width={16}
+            height={16}
+          />
+          <p className="tiny-medium text-light-2">{link.label}</p>
+        </Link>
+      ))}
+    </footer>
+  )
 }
 
 export default Bottombar
